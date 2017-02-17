a851ef75b665bcc72c6f8ba917f57e3a717ea71e
Dmitry Harnitski <dharnitski@gmail.com>
2017-02-16 22:47:53 -0500

    Sending a POST Request

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,4 +1,4 @@
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/Rx';
 
@@ -10,6 +10,15 @@ export class HttpService {
   getData() {
     return this.http.get('https://angular2-course-1621f.firebaseio.com/title.json')
       .map((response: Response) => response.json());
+  }
+
+  sendData(user: any) {
+    const body = JSON.stringify(user);
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('https://angular2-course-1621f.firebaseio.com/data.json', body,
+      { headers: headers } )
+      .map((data: Response) => data.json());
   }
 
 }
