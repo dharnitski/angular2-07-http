f5a6c4b14ed1ec2aa40d7b98df2271a44204cdf7
Dmitry Harnitski <dharnitski@gmail.com>
2017-02-16 22:32:07 -0500

    use Oservable map()

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,5 +1,6 @@
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/Rx';
 
 @Injectable()
 export class HttpService {
@@ -7,7 +8,8 @@ export class HttpService {
   constructor(private http: Http) { }
 
   getData() {
-    return this.http.get('https://angular2-course-1621f.firebaseio.com/title.json');
+    return this.http.get('https://angular2-course-1621f.firebaseio.com/title.json')
+      .map((response: Response) => response.json());
   }
 
 }
